704910a764aa31232eb33613c5d3cd126a818db5
somename <someemail>
2015-10-22 11:41:54 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
+	public void testPlanetSizeHundredByHundredNoObsticles() {
 		fail("Not yet implemented");
 	}
 }
